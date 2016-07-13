619e9de3aa8052790c4d7382058b617ae8f475cc
Mario Oliveira <majroliv@gmail.com>
2016-07-13 17:11:32 -0300

    [webApp] - Requisicoes ajax

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+// Botao para trocar o modo de exibicao dos cards
 var botao = document.querySelector("#mudaLayout");
 botao.addEventListener("click", function () {
 
@@ -12,6 +13,7 @@ botao.addEventListener("click", function () {
 
 });
 
+// Remover cartão
 var botoes = document.querySelectorAll(".opcoesDoCartao-remove");
 for (var i = 0; i < botoes.length; i = i + 1) {
 	botoes[i].addEventListener("click", removeCartao)
@@ -27,7 +29,7 @@ function removeCartao() {
 }
 
 // Decide o tipo de cartao
-function decideTipoDoCartao (conteudo) {
+function decideTipoCartao (conteudo) {
 	var qtdaChar = conteudo.replace(/<br>/g, "").length;
 	var linhas = conteudo.split("<br>").length;
 	var palavras = conteudo.replace(/<br>/g, "").split("");
@@ -40,7 +42,7 @@ function decideTipoDoCartao (conteudo) {
 	});
 
 	var tipo = "cartao--fontePequena";
-	if (qtdaChar < 40 && linhas < 3 && maior.length < 6) {
+	if (qtdaChar < 20 && linhas < 3 && maior.length < 6) {
 		tipo = "cartao--fonteGrande";
 	} else if (qtdaChar < 50 && linhas < 6 && maior.length < 10){
 		tipo = "cartao--fonteMedia";
@@ -48,7 +50,29 @@ function decideTipoDoCartao (conteudo) {
 	return tipo;
 }
 
+// adicona cartao
+function adicionaCartao (conteudo, cor) {
+	contador++;
+
+	var remove = $("<button>").text("Remover").attr("data-ref", contador)
+	.addClass("opcoesDoCartao-opcao").addClass("opcoesDoCartao-remove")
+	.click(removeCartao);
+
+	var opcoes = $("<div>").addClass("opcoesDoCartao").append(remove);
+
+	var p = $("<p>").addClass("cartao-conteudo").html(conteudo);
+
+	// chamada para nova função
+	var tipo = decideTipoCartao(conteudo);
+
+	$("<div>").addClass("cartao").addClass(tipo).attr("id", "cartao_" + contador)
+	.append(opcoes).css("background-color", cor).append(p).prependTo(".mural");
+}
+
+// Verifica qtda de cartoẽs e grava novo cartao
 var contador = $(".cartao").length;
+
+// Novo cartao
 $(".novoCartao").submit(function (e) {
 	e.preventDefault();
 
@@ -57,21 +81,89 @@ $(".novoCartao").submit(function (e) {
 	.replace(/\*(.*)\*/, '<em>$1</em>');
 
 	if (conteudo) {
-		contador++;
-
-		var remove = $("<button>").text("Remover").attr("data-ref", contador)
-		.addClass("opcoesDoCartao-opcao").addClass("opcoesDoCartao-remove")
-		.click(removeCartao);
-
-		var opcoes = $("<div>").addClass("opcoesDoCartao").append(remove);
-
-		var p = $("<p>").addClass("cartao-conteudo").html(conteudo);
-
-		var tipo = decideTipoDoCartao(conteudo);
-
-		$("<div>").addClass("cartao").addClass(tipo).attr("id", "cartao_" + contador)
-		.append(opcoes)
-		.append(p).prependTo(".mural");
+		adicionaCartao(conteudo);
 	}
 	campo.val("");
 })
+
+//Busca de texto dos cartões
+$("#busca").on("input", function () {
+	//Guarda o valor digitado, removendo espaços extras
+	var busca = $(this).val().trim();
+
+	if (busca) {
+		$(".cartao").hide().filter(function () {
+			var conteudo = $(this).find(".cartao-conteudo").text();
+			var regex = new RegExp(busca, "i");
+
+			return conteudo.match(regex);
+		}).show();
+	} else {
+		$(".cartao").show();
+	}
+})
+
+//Requisicao assincrona
+$("#ajuda").click(function () {
+	$.getJSON("https://ceep.herokuapp.com/cartoes/instrucoes",
+		function (res) {
+			var instrucoes = res.instrucoes;
+			instrucoes.forEach(function (instrucao) {
+				adicionaCartao(instrucao.conteudo, instrucao.cor);
+			}) 
+		}
+	);
+})
+
+
+//Retorna dados salvos
+var usuario = "majroliv@gmail.com";
+
+//Salvar cartoes
+$("#salvar").click(function () {
+	$(this).addClass("botaoSync--anima");
+	$("#salvar").removeClass("botaoSync--error");
+	$("#salvar").removeClass("botaoSync--success");
+
+	var cartoes = [];
+
+	$(".cartao").each(function () {
+		var conteudo = $(this).find(".cartao-conteudo").text();
+
+		var cartao = {};
+		cartao.conteudo = conteudo;
+		cartoes.push(cartao);
+	});
+	// ecolha seu nome
+	var mural = {
+		usuario: usuario,
+		cartoes: cartoes
+	}
+
+	$.ajax({
+		url: "https://ceep.herokuapp.com/cartoes/salvar",
+		method: "post",
+		data: mural,
+		success: function (res) {
+			$("#salvar").addClass("botaoSync--success"); 
+			console.log(res.quantidade);
+		},
+		error: function () {
+			$("#salvar").addClass("botaoSync--error"); 
+		},
+		complete: function () {
+			$("#salvar").removeClass("botaoSync--anima");
+		}
+	})
+})
+
+
+$.getJSON("https://ceep.herokuapp.com/cartoes/carregar?callback=?",
+	{usuario : usuario},
+	function (res) {
+		var cartoes = res.cartoes;
+		console.log(cartoes.length + " carregados em " + res.usuario);
+		cartoes.forEach(function (cartao) {
+			adicionaCartao(cartao.conteudo);
+		});
+	});
